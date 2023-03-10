df70ced5e4aa19149efbaffb6bac9353ccfefbbc
hosy <hosy@nurigo.net>
2023-03-10 16:26:00 +0900

    Update createSSOToken.php

--- examples/createSSOToken.php
+++ examples/createSSOToken.php
@@ -2,7 +2,7 @@
 require_once("../lib/sso.php");
 
 $params = array(
-  "appId" =>  "##APP_ID##", // 회원가입 및 SSO Token의 주체가 되는 앱 아이디
+  "appId" => "##APP_ID##", // 회원가입 및 SSO Token의 주체가 되는 앱 아이디
   "email" => "##MEMBER_EMAIL##", // 회원가입에 사용될 이메일 주소
   "password" => "##MEMBER_PASSWORD#", // 회원가입에 사용될 암호
   "customerKey" => "##MEMBER_CUSTOMER_KEY##" // 회원 구분 키 (이미 가입된 회원일 경우 사용되지 않음)
